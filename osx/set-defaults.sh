eaed3eba23906555d02b516dbcbc471081e8ba05
Ted Suzman <ted@suzman.net>
2012-06-18 22:38:41 -0700

    /my/ osx settings

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -2,19 +2,13 @@
 #
 # Or, in other words, set shit how I like in OS X.
 #
-# The original idea (and a couple settings) were grabbed from:
+
+# Grab whatever settings you want from here:
 #   https://github.com/mathiasbynens/dotfiles/blob/master/.osx
 #
 # Run ./set-defaults.sh and you'll be good to go.
 
-# Disable press-and-hold for keys in favor of key repeat
-defaults write -g ApplePressAndHoldEnabled -bool false
-
-# Use AirDrop over every interface. srsly this should be a default.
-defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+# More settings:
 
 # Always open everything in Finder's list view. This is important.
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
-
-# Show the ~/Library folder
-chflags nohidden ~/Library
